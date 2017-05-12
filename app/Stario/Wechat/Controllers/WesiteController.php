a6007eb1585fbf7005409605088bc2ba7a773fa3
hulume <partoo.cn@gmail.com>
2017-05-12 21:42:51 +0800

    init

--- app/Stario/Wechat/Controllers/WesiteController.php
+++ app/Stario/Wechat/Controllers/WesiteController.php
@@ -11,7 +11,7 @@ class WesiteController extends Controller {
 
 	private $settings;
 	public function __construct(Wesite $wesite) {
-		$this->settings = $wesite->first()->settings();
+		// $this->settings = $wesite->first()->settings();
 	}
 
 	public function linkMaterial(Request $request) {
@@ -19,7 +19,7 @@ class WesiteController extends Controller {
 		$material = EasyWeChat::material();
 		foreach ($thumb_ids as $id) {
 			$thumb = $material->get($id);
-			response()->download(public_path() . '/wesite/', $thumb, $headers);
+			file_put_contents(public_path() . '/upload/wechat' . $id, $thumb);
 		}
 		// $thumb = $material->get();
 	}
