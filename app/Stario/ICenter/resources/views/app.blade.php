afda5bd51e78004e5ad7f4c590e4777e2fe6deeb
hulume <partoo.cn@gmail.com>
2017-05-12 20:05:56 +0800

    init

--- app/Stario/ICenter/resources/views/app.blade.php
+++ app/Stario/ICenter/resources/views/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="shortcut icon" href="/favicon.ico" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Wemesh') }}</title>
-    <link href={{assets('css/app.css')}} rel="stylesheet">
+    <link href={{asset('css/app.css')}} rel="stylesheet">
     <script>
         window.Wemesh = {csrfToken: "{{ csrf_token() }}", id: "{{ Auth::id()}}"}
         window.app = "{{config('app.name', 'Wemesh')}}"
@@ -19,7 +19,7 @@
 </head>
 <body class="skin-blue fixed">
     <div id="app"></div>
-    <script src={{assets('js/app.js')}}></script>
+    <script src={{asset('js/app.js')}}></script>
     @yield('scripts')
 </body>
 </html>
